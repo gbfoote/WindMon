e53eb538e04ed5b24dff846314c6395c8fc8dd55
George Foote <gbfoote@advantec.com>
2016-03-21 21:01:31 -0400

    WindAnalysis.py - added averaging

--- WindAnalysis.py
+++ WindAnalysis.py
@@ -5,6 +5,8 @@ import time
 
 fileName = 'Wind.bin'
 arraySize = 60
+recordLen = 8       # File data block size
+velocityThreshold = 0.020       # Seconds = > 115 mph
 
 # Turbine power curve
 power = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.24, 0.35, 0.43, 0.65,
@@ -13,7 +15,7 @@ power = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.24, 0.35, 0.43, 0.65,
 electricRate = 0.17 # $/kWh
 
 def fetchRecord(file):
-    recordLen = 8
+    global recordLen
     t1 = 0
     OK = True
     while True:
@@ -56,6 +58,10 @@ def summarizePwrByTick():
                 pass
                 #print(time.asctime(time.localtime(t))+' {0:6.4f} {2:5d} {1:5d}'.format(dt, vel, lastVel))
             lastVel = vel
+    dispPwr(a)
+
+def dispPwr(a):
+    global power
     i = 0
     s = sum(a)
     s1 = 0.0
@@ -72,12 +78,78 @@ def summarizePwrByTick():
     print ('Average power is: {0:6.3f} kW or {1:6.3f} mWh/yr or {2:6.0f} $/yr'\
             .format(s1, s1 * hoursPerYear / 1000, s1 * hoursPerYear * electricRate))
 
+def readASecond(file, t0):
+    global recordLen
+    OK = True
+    ticks = 0
+    while True:
+        record = file.read(recordLen)
+        if len(record) < recordLen:
+            OK = False
+            break
+        if (record[7]== 0x80):       # This is a velocity tick9' Steel W10x54 I-Beam
+            sec = int.from_bytes(record[0:4], 'little')
+            ms = int.from_bytes(record[4:6], 'little')
+            t = sec + ms/10000.0
+            dt = t - t0
+            if dt > velocityThreshold:
+                ticks += 1
+                t0 = t
+        else:
+            break
+    return (OK, ticks, t0)
+
 def averagedPwr():
-    pass
+
+    global recordLen
+    averagingTime = 30
+    t0 = 0
+    f = open(fileName, 'rb')
+    tickList = []
+    tickSum = 0
+
+    bin = []
+    noBins = 60             # 1 mph bins
+    for i in range(noBins): # Initialize bins
+        bin.append(0)
+
+    # Scan to first direction data block
+    while True:
+        record = f.read(recordLen)
+        sec = int.from_bytes(record[0:4], 'little')
+        ms = int.from_bytes(record[4:6], 'little')
+        t0 = sec + ms/10000.0
+        if not (record[7]== 0x80):
+            break
+
+    # Read averagingTime direction records counting ticks and storing into list
+    for i in range(averagingTime):
+        (OK, ticks, t0) = readASecond(f, t0)
+        tickList.append(ticks)
+        tickSum += ticks
+
+    # Process data a second at a time
+    while OK:
+        (OK, ticks, t0) = readASecond(f, t0)
+        if OK:
+            tickSum -= tickList[0]
+            for i in range(averagingTime - 1):        # remove oldest from sum
+                tickList[i] = tickList[i + 1]   # shift the list down
+            tickList[averagingTime - 1] = ticks
+            tickSum += ticks
+            aveVel = int((tickSum * 2.33 / averagingTime) + 0.5)
+            if aveVel < noBins:
+                bin[aveVel] += 1
+    dispPwr(bin)
+
+
+
+
+
 
 def erroniousTicks():
 
-    threshold = 0.03    # ms
+    threshold = 0.02    # ms
     f = open(fileName, 'rb')
     noTicks = 5
     cnt = 0
@@ -104,8 +176,8 @@ def erroniousTicks():
 
 def main():
     # summarizePwrByTick()
-    erroniousTicks()
-
+    # erroniousTicks()
+    averagedPwr()
 
 if __name__ == '__main__':
     main()
